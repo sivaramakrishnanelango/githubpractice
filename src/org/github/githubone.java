f0dd533353a61d20a3a7c78cbdb9fb2b9b0025d6
sivaramakrishnan <sivaram.e98@gmail.com>
2022-08-22 16:17:45 +0530

    practice - local to remote

---
+++ src/org/github/githubone.java
@@ -0,0 +1,20 @@
+package org.github;
+
+public class githubone {
+	
+	private void car() {
+		System.out.println("i like my toyota car");
+
+	}
+	
+	private void bus() {
+		System.out.println("i like to travel in volvo bus");
+
+	}
+public static void main(String[] args) {
+	
+	githubone g = new githubone();
+	g.car();
+	g.bus();
+}
+}
